a177fe427869335269fb27128e68f1aec99bb01d
Ben <uncleben006@gmail.com>
2016-08-27 03:44:15 +0800

    8/27建檔

--- system/checkuser.php
+++ system/checkuser.php
@@ -1,7 +1,67 @@
 <?php
-include('link.php');
-$account = $_POST["account"];
-$password = $_POST["password"];
 
-$setSQL = "SELECT * FROM `` "
+// 1 是代表user，例如setSQL1、result1
+// 2 是代表admin，例如setSQL2、result2
+
+include('link.php');
+mysql_query("SET NAMES'UTF8'");
+mysql_query("SET CHARACTER SET UTF8");
+mysql_query("SET CHARACTER_SET_RESULTS='UTF8'");
+
+//上面三行mysql_query是為了資料存入SQL時轉UTF8
+
+$account = $_POST["account"];
+$pass = $_POST["password"];
+
+//這支程式要嘛搜尋到 user 要嘛搜尋到 admin 然後產生 session
+
+$setSQL1 = "SELECT * FROM `user` WHERE `account` = '" . $account . "' and `password` = '" . $pass . "'";
+$result1 = mysql_query($setSQL1);
+$row1 = mysql_fetch_assoc($result1);
+session_set_cookie_params(600);
+session_start();
+$_SESSION["no"] = $row1["no"];
+$_SESSION["pri"] = $row1["pri"];
+$_SESSION["account"] = $row1["account"];
+$_SESSION["password"] = $row1["password"];
+$_SESSION["name"] = $row1["name"];
+$_SESSION["email"] = $row1["email"];
+$_SESSION["introduction"] = $row1["introduction"];
+session_write_close();
+$url = "testlogin.php?name=".$_SESSION["name"]."&pri=".$_SESSION["pri"];
+echo "<script type='text/javascript'>";
+echo "window.location.href='$url'";
+echo "</script>"; 
+
+$setSQL2 = "SELECT * FROM `admin` WHERE `account` = '" . $account . "' and `password` = '" . $pass . "'";
+$result2 = mysql_query($setSQL2);
+$row2 = mysql_fetch_assoc($result2);
+session_set_cookie_params(600);
+session_start();
+$_SESSION["no"] = $row1["no"];
+$_SESSION["pri"] = $row1["pri"];
+$_SESSION["account"] = $row1["account"];
+$_SESSION["password"] = $row1["password"];
+$_SESSION["name"] = $row1["name"];
+$_SESSION["email"] = $row1["email"];
+$_SESSION["introduction"] = $row1["introduction"];
+session_write_close();
+$url = "testlogin.php?name=".$_SESSION["name"]."&pri=".$_SESSION["pri"];
+echo "<script type='text/javascript'>";
+echo "window.location.href='$url'";
+echo "</script>"; 
+
+
+
+
+
+/*
+echo $row1["pri"];
+echo $row1["no"];
+echo $row1["account"];
+echo $row1["password"];
+echo $row1["name"];
+echo $row1["email"];
+echo $row1["introduction"];
+*/
 ?>
