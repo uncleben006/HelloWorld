8e6c217c7be4ae0bb69753d6aaa3ac2658052843
Ben <uncleben006@gmail.com>
2016-08-30 14:15:13 +0800

    8/30:登入href連至system/user/login.php

--- system/logout.php
+++
@@ -1,9 +0,0 @@
-<?php 
-        session_unset(); 
-        session_destroy();
-        // destroy the session  
-        $url = "http://localhost:8080/JOMO/index6.html";
-        echo "<script type='text/javascript'>";
-        echo "window.location.href='$url'";
-        echo "</script>"; 
-?>
