cfe8dd8ca31cc08760efd1ca1be06489b47e77a2
Ben <uncleben006@gmail.com>
2016-09-08 21:44:28 +0800

    9/8:修改，資料庫匯出中文亂碼問題

--- system/user/confirm.php
+++ system/user/confirm.php
@@ -1,10 +1,14 @@
 <?php
+header("Content-Type:text/html; charset=utf-8");
 include('link.php');
 
 $no = $_GET["no"];
 $setSQL = "UPDATE `user` SET `pri`='1' WHERE `no`='".$no."'";
 echo $setSQL;
 mysql_query($setSQL);
+mysql_query("SET NAMES'UTF8'");
+mysql_query("SET CHARACTER SET UTF8");
+mysql_query("SET CHARACTER_SET_RESULTS='UTF8'");
 
 //功能:從資料庫抓取該用戶的資料並產生session
 //目的:避免用戶因為隔太久未驗證導致session消失的狀況，確保index.php的linkdb.php一定能取得session
@@ -22,7 +26,6 @@ $_SESSION["name"] = $row1["name"];
 $_SESSION["email"] = $row1["email"];
 $_SESSION["introduction"] = $row1["introduction"];
 session_write_close();
-
 echo '<h1>驗證完成</h1><br>';
 echo '你已經信箱驗證並開通了你的帳號，現在你帳號擁有更多的權限<br>';
 echo '<a href="http://localhost:8080/JOMO/index6.php"><button>好的非常感謝</button></a>';
