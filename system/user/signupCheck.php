67bbd0ba1dcbf3d8e980a8eb3a10438ebd5e9d13
Ben <uncleben006@gmail.com>
2016-09-08 21:48:18 +0800

    9/6:修改，能上傳圖片

--- system/user/signupCheck.php
+++ system/user/signupCheck.php
@@ -11,8 +11,37 @@ $repass = $_POST["repass"];
 $name = $_POST["name"];
 $email = $_POST["email"];
 $introduction = $_POST["introduction"];
-$pic = $_POST["pic"];
-$pri = 0;
+$pri = '0';
+
+$imgFile = $_FILES['pic']['name'];//取得FILE名稱
+$tmp_dir = $_FILES['pic']['tmp_name'];//把FILE路徑暫存入一個tmp檔
+$imgSize = $_FILES['pic']['size'];//取得FILE大小
+		
+if(empty($imgFile)!=TRUE){//這裡不知道為什麼用isset不行
+	$upload_dir = 'photo/'; // upload directory
+	
+	$imgExt = strtolower(pathinfo($imgFile,PATHINFO_EXTENSION)); // get image extension，回傳圖片的檔案型態(.jpg/.png)(extension:延伸部分)
+		
+	// valid image extensions(有效圖片型態)
+	$valid_extensions = array('jpeg', 'jpg', 'png', 'gif'); //宣告有效的檔案型態
+		
+	//重新命名上傳的圖片
+	$userpic = rand(1000,1000000).".".$imgExt; //產生文件名稱並指定給$userpic
+				
+	// allow valid image file formats
+	if(in_array($imgExt, $valid_extensions)){ //若在$imgExt裡有$valid_extensions的檔案型態的話，就執行			
+		// Check file size '5MB' 
+		if($imgSize < 5000000){
+			move_uploaded_file($tmp_dir,$upload_dir.$userpic); //把tmp暫存檔存入指定位置($upload_dir+$userpic)(位置加檔名)
+		}
+		else{
+			$errMSG = "呀啊啊啊~太大了啦~人家受不了啦~我是指檔案";
+		}
+	}
+	else{
+		$errMSG = "抱歉...我只喜歡附檔名是JPG, JPEG, PNG或GIF";		
+	}
+}
 
 $getAllrowsSQL="SELECT COUNT(*) FROM user";
 $runSQL=mysql_query($getAllrowsSQL);
@@ -24,7 +53,7 @@ else if(strlen($nowRow)==2)
 	$no = '0' . $nowRow;
 else
 	$no = $nowRow;
-for($i=0;$i<2;$i++)
+for($i=0;$i<2;$i++)//產生亂碼no
 {
 	$j = rand(1,26);
 	switch($j)
@@ -121,12 +150,17 @@ $_SESSION["repass"] = $repass;
 $_SESSION["name"] = $name;
 $_SESSION["email"] = $email;
 $_SESSION["introduction"] = $introduction;
-$_SESSION["pic"] = $pic;
+$_SESSION["photo"] = $userpic;//把產生的亂數圖片檔名做成session，好之後傳入資料庫。
+$_SESSION["errMSG"] = $errMSG;
 
 $sql = "SELECT * FROM `user` WHERE `account`='".$account."'";
 $result = mysql_query($sql);
 $row = mysql_fetch_assoc($result);
 
+
+//value1=除了密碼之外其他都顯示
+//value2=除了密碼信箱之外其他都顯示
+//wrong1,2,3分別顯示:密碼未重複,email未正確填寫,帳號已有人填過
 if($pass!=$repass){
 	$url = "signup.php?value=1&wrong=1";
 	echo "<script type='text/javascript'>";
@@ -141,6 +175,12 @@ else if(!preg_match("/([\w\-]+\@[\w\-]+\.[\w\-]+)/",$email)){//判斷有沒有em
 }
 else if(isset($row["account"])){//判斷帳號重複
 	$url = "signup.php?value=1&wrong=3";
+	echo "<script type='text/javascript'>";
+	echo "window.location.href='$url'";
+	echo "</script>";
+}
+else if(isset($errMSG)){
+	$url = "signup.php?value=1&wrong=4";
 	echo "<script type='text/javascript'>";
 	echo "window.location.href='$url'";
 	echo "</script>";
