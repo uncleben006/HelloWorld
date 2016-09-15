0d22b217a2401707e4f18b2d006504926bb0999b
Ben <uncleben006@gmail.com>
2016-09-16 01:01:43 +0800

    link database

---
+++ system/group/link.php
@@ -0,0 +1,20 @@
+<?php
+$user = 'root';
+$pass = 'ken0426';
+$host = 'localhost';
+
+$link = mysql_connect($host, $user, $pass);
+
+if(!$link)
+{
+	die('can not connect to MySQL' . mysql_errno());	
+}
+
+$database = "boardgame";
+$db_select = mysql_select_db($database);
+
+if(!$db_select)
+{
+	die('can not connect to ' . $database . ' ' . mysql_errno());	
+}
+?>
