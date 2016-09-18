ccd2163a4fe8e254150a4f6dbdf97736cdfc5fa1
Ben <uncleben006@gmail.com>
2016-09-18 14:16:55 +0800

    俐孟寫的js(mouseon mouseout)

---
+++ javascript.js
@@ -0,0 +1,36 @@
+/*大富翁圖片按鈕-店家地圖*/
+function mouseOver1()
+{
+document.getElementById('a1').src ="jomor_html/img/button00.png"
+}
+function mouseOut1()
+{
+document.getElementById('a1').src ="jomor_html/img/button01.png"
+}
+/*大富翁圖片按鈕-我要揪團*/
+function mouseOver2()
+{
+document.getElementById('a2').src ="jomor_html/img/button03.png"
+}
+function mouseOut2()
+{
+document.getElementById('a2').src ="jomor_html/img/button02.png"
+}
+/*大富翁圖片按鈕-桌遊專欄*/
+function mouseOver3()
+{
+document.getElementById('a3').src ="jomor_html/img/button05.png"
+}
+function mouseOut3()
+{
+document.getElementById('a3').src ="jomor_html/img/button04.png"
+}
+/*大富翁圖片按鈕-我要討論*/
+function mouseOver4()
+{
+document.getElementById('a4').src ="jomor_html/img/button07.png"
+}
+function mouseOut4()
+{
+document.getElementById('a4').src ="jomor_html/img/button06.png"
+}
