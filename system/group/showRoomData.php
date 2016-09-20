d4cc54949273b2588b0a1421135d7f7ac17a6883
Ben <uncleben006@gmail.com>
2016-09-21 03:01:11 +0800

    增加判斷房間人數

--- system/group/showRoomData.php
+++ system/group/showRoomData.php
@@ -3,7 +3,8 @@ include('link.php');
 session_start();
 $no=$_GET['no'];
 
-$mysql2 = "
+//要是room+num不存在，則建立資料庫
+$mysql = "
 CREATE TABLE IF NOT EXISTS `room".$no."`(
 	`people` int(2) NOT NULL,
 	`name` text NOT NULL,
@@ -12,7 +13,7 @@ CREATE TABLE IF NOT EXISTS `room".$no."`(
 	PRIMARY KEY(`account`)
 )DEFAULT CHARSET = UTF8;
 ";
-mysql_query($mysql2);
+mysql_query($mysql);
 
 $mysql1 = "SELECT * FROM `room` WHERE `no` ='$no'";
 mysql_query("SET NAMES'UTF8'");
@@ -21,7 +22,13 @@ mysql_query("SET CHARACTER_SET_RESULTS='UTF8'");
 $result1 = mysql_query($mysql1);
 $row1 = mysql_fetch_assoc($result1);
 
+//抓取room+num裡的列數，給num2
+$mysql2 = "SELECT * FROM `room".$no."`";
+$result2 = mysql_query($mysql2);
+$number = mysql_num_rows($result2);
+
 $room = $row1['room'];
+$host = $row1['host'];
 $store = $row1['store'];
 $game = $row1['game'];
 $date = $row1['date'];
@@ -30,9 +37,18 @@ $people = $row1['people'];
 $spend = $row1['spend'];
 $remark = $row1['remark'];
 
+
 if(isset($_POST['join'])){
 	if(empty($_SESSION['account'])){
 		header("Location:../user/block.php");
+	}
+	else if($number>=$people){
+	//若房裡人數小於房主宣告人數，才能執行insert動作
+		?>
+		<script type="text/javascript">
+			alert("抱歉人數已滿~~去加別房啦~");
+		</script>
+		<?php
 	}
 	else{
 		$uno = $_SESSION['no'];
@@ -43,11 +59,11 @@ if(isset($_POST['join'])){
 		$photo = $_SESSION['photo'];
 
 
-		$mysql2 = 'INSERT INTO `room'.$no.'`(`people`, `name`, `account`, `photo`) VALUES ("'.$people.'","'.$name.'","'.$account.'","'.$photo.'")';
+		$mysql3 = 'INSERT INTO `room'.$no.'`(`people`, `name`, `account`, `photo`) VALUES ("'.$people.'","'.$name.'","'.$account.'","'.$photo.'")';
 		mysql_query("SET NAMES'UTF8'");
 		mysql_query("SET CHARACTER SET UTF8");
 		mysql_query("SET CHARACTER_SET_RESULTS='UTF8'");
-		mysql_query($mysql2);
+		mysql_query($mysql3);
 
 		$addMember;
 	}
@@ -62,149 +78,24 @@ if(isset($_POST['join'])){
 <head>
 	<title>showRoomData</title>
 	<meta charset="UTF-8">
+	<link rel="stylesheet" type="text/css" href="group.css">
 	<style type="text/css">
-		.frame{
-			width: 720px;
-			margin-right: auto;
-			margin-left: auto;
-			padding-top: 20px;
-		}
-		input[type="text"],input[type="password"],input[type="date"],input[type="time"],input[type="number"]{
-			border-color:#bdc5d0;
-		}
-		table{
-			height: 80px;
-		}
-		.room{
-			float: left;
-			width:100%;
-			margin:15px;
-		}
-		.room_padding{
-			background-color: #f2e392;
-			border-radius: 15px 15px 0px 0px;
-			padding: 45px 15px 5px 15px;
-		}
-		.room_content{
-			width: 100%;
-			margin:0 auto;
-		}
-		.room_banner{
-			width: 100%;
-			float: left;
-		}
+		
 
-		.room_banner1{
-			background-color: #e7bbb2;
-			width: 15%;
-			border-radius: 15px 0px 0px 0px ;
-			padding-top: 5px;
-			float: left;
-			text-align:center;
-			font-size: 20px;
-			height: 30px;
-		}
-		.room_banner2{
-			background-color: #cc3e3e;
-			width: 85%;
-			border-radius: 0px 15px 0px 0px ;
-			padding-top: 5px;
-			float: left;
-			text-align: center;
-			font-size: 20px;
-			height: 30px;
-		}
-		.room_title{
-			margin:5px 0px 5px 0px;
-		}
-		.room_title1{
-			background-color: #e7bbb2;
-		    text-align: center;
-		    padding: 5px;
-		    height: 20px;
-		    border-radius: 12px;
-		    width: 25%;
-		    margin-right: 5px;
-		    float:left;
-
-		}
-		.room_title2{
-			background-color: #ffffff;
-			padding: 5px;
-			height: 20px;
-			border-radius: 12px;
-
-		}
-		.room_footer{
-			height: 30px;
-			position: relative;
-			width:100%;
-		}
-		.room_footer_redline{
-			background-color: #eb6063;
-			width: 100%;
-			height: 7px;
-
-		}
-		.room_footer1{
-			background-color: #4fbcc1;
-			width: 100%;
-			border-radius: 0px 0px 15px 15px ;
-			padding:5px 0px 5px 0px;
-			float: left;
-			font-size: 20px;
-			height: 30px;
-			text-align: center;
-
-		}
-		.room_footer_bottom{
-			color:black;
-			border-radius: 12px;
-			font-color:#4fbcc1;
-			font-size: 15px;
-		}
-		.member{
-			float: left;
-			width:220px;
-			margin:10px;
-		}
-		.member_banner1{
-			background-color: #e7bbb2;
-			width: 30%;
-			border-radius: 15px 0px 0px 0px ;
-			padding-top: 5px;
-			float: left;
-			text-align:center;
-			font-size: 20px;
-			height: 30px;
-		}
-		.member_banner2{
-			background-color: #cc3e3e;
-			width: 70%;
-			border-radius: 0px 15px 0px 0px ;
-			padding-top: 5px;
-			float: left;
-			text-align: center;
-			font-size: 20px;
-			height: 30px;
-		}
-		.member_title{
-			margin:5px 0px 10px 0px;
-		}
-		.member_title2{
-			background-color: #ffffff;
-			padding: 5px;
-			border-radius: 12px;
-		}
-	</style>
-
+		
+	</style>	
 </head>
 <body>
 	<div class="frame">
-
+			
+		<h1>這是<?php echo $no."號房間"  ?></h1>
+		<div style="float:right">
+			<a href="group.php">
+				<button>回揪團頁</button>
+			</a>
+		</div>
 		<form method="post">
-			<h1>這是<?php echo $no."號房間"  ?></h1>
-			<div class="room">
+			<div class="room2">
 					<div class="room_banner">
 						<div class="room_banner1"><?php echo $row1['no'] ?></div>
 						<div class="room_banner2"><?php echo $row1['room'] ?></div>
@@ -212,32 +103,36 @@ if(isset($_POST['join'])){
 					<div class="room_padding">
 						<div class="room_content">
 							<div class="room_title">
+								<div class="room_title1">房主</div>
+								<div class="room_title3"><?php echo $host ?></div>
+							</div>
+							<div class="room_title">
 								<div class="room_title1">日期</div>
-								<div class="room_title2"><div><?php echo $row1['date'] ?></div></div>
+								<div class="room_title3"><?php echo $row1['date'] ?></div>
 							</div>
 							<div class="room_title">
 								<div class="room_title1">時間</div>
-								<div class="room_title2"><?php echo $row1['time'] ?></div>
+								<div class="room_title3"><?php echo $row1['time'] ?></div>
 							</div>
 							<div class="room_title">
 									<div class="room_title1">地點</div>
-								<div class="room_title2"><?php echo $row1['store'] ?></div>
+								<div class="room_title3"><?php echo $row1['store'] ?></div>
 							</div>
 							<div class="room_title">
 								<div class="room_title1">人數</div>
-								<div class="room_title2"><?php echo $row1['people'] ?></div>
+								<div class="room_title3"><?php echo $row1['people'] ?></div>
 							</div>
 							<div class="room_title">
 								<div class="room_title1">遊戲</div>
-								<div class="room_title2"><?php echo $row1['game'] ?></div>
+								<div class="room_title3"><?php echo $row1['game'] ?></div>
 							</div>
 							<div class="room_title">
-								<div class="room_title1">遊戲</div>
-								<div class="room_title2"><?php echo $row1['spend'] ?></div>
+								<div class="room_title1">計費方式</div>
+								<div class="room_title3"><?php echo $row1['spend'] ?></div>
 							</div>
 							<div class="room_title">
-								<div class="room_title1">遊戲</div>
-								<div class="room_title2"><?php echo $row1['remark'] ?></div>
+								<div class="room_title1">備註</div>
+								<div class="room_title3"><?php echo $row1['remark'] ?></div>
 							</div>
 						</div>
 					</div>	
@@ -260,7 +155,15 @@ if(isset($_POST['join'])){
 				?>
 				<div class="member">
 					<div class="room_banner">
-						<div class="member_banner1"><?php echo "帳號" ?></div>
+					<?php
+					if($host==$row3['account']){
+						echo '<div class="member_banner1">房主</div>';
+					}
+					
+					else{			
+						echo '<div class="member_banner1">帳號</div>';
+					}
+					?>	
 						<div class="member_banner2"><?php echo $row3['account'] ?></div>
 					</div>
 					<div class="room_padding">
@@ -272,7 +175,7 @@ if(isset($_POST['join'])){
 							</div>
 							<div class="room_title">
 								<div class="room_title1">暱稱</div>
-								<div class="room_title2"><?php echo $row3['name'] ?></div>
+								<div class="room_title4"><?php echo $row3['name'] ?></div>
 							</div>
 						</div>
 					</div>	
