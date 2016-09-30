9a392f426a1e01dbfb00397bd61328406b106579
Ben <uncleben006@gmail.com>
2016-09-30 13:17:47 +0800

    include header.php

---
+++ aboutus.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>jomor桌末狂歡</title>
+	<link href="style.css" rel="stylesheet" type="text/css" />
+	<link rel="stylesheet" type="text/css" href="flexslider.css" />
+	<meta charset="utf-8">
+</head>
+	<body id="body0">
+		<!--把header放入header.php方便管理-->
+		<?php include("header.php"); ?>
+		<section class="about_blue_section"><!--藍底區塊-->
+			<div class="about_blue_bg"><!--團隊介紹和信箱-->
+				<img class="aboutus_img" src="jomor_html/img/aboutus_img.png">
+				<p class="aboutus_p">我們是一群熱愛桌遊的大學生，希望能透過我們所學的專業，將桌遊的益處推廣出去，並開發新的桌遊資訊平台，其中包括桌遊店家的詳細資訊與地圖、揪團功能、桌遊專欄、討論區等，希望提供多方面的桌遊資訊平台，讓桌遊變得更平易近人，因而吸引更多玩家加入美好的桌遊世界。
+				</p>
+				<p class="aboutus_p2">網站上使用有任何問題請寄信至<a class="mail" href="mailto:ics.jomorparty@gmail.com">ics.jomorparty@gmail.com</a>
+				</p>
+			</div>
+		</section>
+		<section>
+			<div class="aboutus_photo_div1"><!--個人介紹表格-->
+				<div class="aboutus_tr"><!--tr-->
+					<div class="aboutus_td"><!--td-->
+						<img src="jomor_html/img/aboutus_photo.png" height="350px" width="200px">
+					</div>
+					<div class="position">專案<br>經理</div><!--td-->
+					<div class="work_td">
+						<div class="work1">
+							<p class="work_p1">&nbsp;&nbsp;&nbsp;預算管理<br>&nbsp;&nbsp;&nbsp;企劃撰寫<br>專案進度管理</p>
+						</div>
+					</div>
+				</div>					
+			</div>
+			<div class="aboutus_photo_div2"><!--個人介紹表格-->
+				<div class="aboutus_tr"><!--tr-->
+					<div class="aboutus_td"><!--td-->
+						<img src="jomor_html/img/aboutus_photo.png" height="350px" width="200px">
+					</div>
+					<div class="position">網頁<br>設計</div><!--td-->
+					<div class="work_td">
+						<div class="work1">
+							<p class="work_p2">&nbsp;&nbsp;&nbsp;網站架構設計<br>&nbsp;&nbsp;&nbsp;前端語言撰寫</p>
+						</div>
+					</div>
+				</div>					
+			</div>
+			<div class="aboutus_photo_div2"><!--個人介紹表格-->
+				<div class="aboutus_tr"><!--tr-->
+					<div class="aboutus_td"><!--td-->
+						<img src="jomor_html/img/aboutus_photo.png" height="350px" width="200px">
+					</div>
+					<div class="position">程式<br>設計</div><!--td-->
+					<div class="work_td">
+						<div class="work1">
+							<p class="work_p2">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;系統設計<br>&nbsp;&nbsp;&nbsp;後端語言撰寫</p>
+						</div>
+					</div>
+				</div>					
+			</div>
+			<div class="aboutus_photo_div2"><!--個人介紹表格-->
+				<div class="aboutus_tr"><!--tr-->
+					<div class="aboutus_td"><!--td-->
+						<img src="jomor_html/img/aboutus_photo.png" height="350px" width="200px">
+					</div>
+					<div class="position">行銷<br>公關</div><!--td-->
+					<div class="work_td">
+						<div class="work1">
+							<p class="work_p2">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;網路行銷<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;活動企劃</p>
+						</div>
+					</div>
+				</div>					
+			</div>
+			<div class="aboutus_photo_div3"><!--個人介紹表格-->
+				<div class="aboutus_tr"><!--tr-->
+					<div class="aboutus_td"><!--td-->
+						<img src="jomor_html/img/aboutus_photo5.png" height="260px" width="200px">
+					</div>
+					<div class="position2">美術<br>設計</div><!--td-->
+					<div class="work_td">
+						<div class="work2">
+							<p class="work_p2">&nbsp;&nbsp;&nbsp;圖文美工編輯<br>&nbsp;&nbsp;&nbsp;網站介面設計</p>
+						</div>
+					</div>
+				</div>					
+			</div>
+		</section>
+</body>
+</html>
---
+++ blog_more.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>jomor桌末狂歡</title>
+	<link href="style.css" rel="stylesheet" type="text/css" />
+	<link rel="stylesheet" type="text/css" href="flexslider.css" />
+	<meta charset="utf-8">
+</head>
+	<body id="body0">
+	
+		<!--把header放入header.php方便管理-->
+		<?php include("header.php"); ?>
+		<section class="blue_section">	<!--藍底大象-->
+			<div class="blue_bg">
+				<img class="blog_img" src="jomor_html/img/blogimg.png" height="275px" width="720px">
+			</div>
+		</section>
+		<section>
+			<div class="blogbutton_frame">	<!--頁籤按鈕-->
+				<a href="#" class="blog_myButton">熱門文章</a>
+				<a href="#" class="blog_myButton">最新文章</a>
+			</div>
+		</section>
+		<!--文章區塊-->
+		<section class="moreessay_section">
+			<div class="moreessay_fram">
+					<div class="moreeassy_green"><!--綠色標題區塊-->
+						<div class="moreeassy_title">新天鵝堡桌遊中推薦新手TOP10桌遊</div>
+					</div>
+					<div class="moreeassy_pink"><!--粉紅日期區塊-->
+						<span class="moreeassy_date">2016/09/19</span>
+						<span class="moreeassy_times">瀏覽次數：</span>
+					</div>
+					<div class="moreeassy_artical_div"><!--文章內文區塊-->
+						<div class="moreeassy_artical">
+							<h1>TOP 10桌遊</h1>
+							<p>xxxxxxxxxxx天要介紹的是專門推薦給新手玩的～～簡單易懂！！！！不用動太多腦，很適合新手入門！！xxxxxxooooooo!@#$%^**......................xxxxxxxxxxx天要介紹的是專門推薦給新手玩的～～簡單易懂！！！！不用動太多腦，很適合新手入門！！xxxxxxooooooo!@#$%^**......................xxxxxxxxxxx天要介紹的是專門推薦給新手玩的～～簡單易懂！！！！不用動太多腦，很適合新手入門！！xxxxxxooooooo!@#$%^**......................
+							</p>
+							<h1>TOP 9桌遊</h1>
+							<p>xxxxxxxxxx天要介紹的是專門推薦給新手玩的～～簡單易懂！！！！不用動太多腦，很適合新手入門！！xxxxxxooooooo!@#$%^**......................xxxxxxxxxxx天要介紹的是專門推薦給新手玩的～～簡單易懂！！！！不用動太多腦，很適合新手入門！！xxxxxxooooooo!@#$%^**......................xxxxxxxxxxx天要介紹的是專門推薦給新手玩的～～簡單易懂！！！！不用動太多腦，很適合新手入門！！xxxxxxooooooo!@#$%^**......................</p>
+						</div>
+					</div>
+					<div class="moreeassy_img_div">
+						<img class="moreeassy_img" src="jomor_html/img/swancafe01.jpg">
+					</div>
+				</div>
+			</div>
+		</section>
+</body>
+</html>
