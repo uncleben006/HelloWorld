12cab99663a824aef5d99bf1bc0ba86997ed5753
Ben <uncleben006@gmail.com>
2016-09-30 13:00:27 +0800

    俐孟的新style!!~

--- javascript.js
+++ javascript.js
@@ -1,36 +1,100 @@
+/*首頁*/
 /*大富翁圖片按鈕-店家地圖*/
 function mouseOver1()
 {
-document.getElementById('a1').src ="jomor_html/img/button00.png"
+	document.getElementById('a1').src ="jomor_html/img/button00.png"
 }
 function mouseOut1()
 {
-document.getElementById('a1').src ="jomor_html/img/button01.png"
+	document.getElementById('a1').src ="jomor_html/img/button01.png"
 }
 /*大富翁圖片按鈕-我要揪團*/
 function mouseOver2()
 {
-document.getElementById('a2').src ="jomor_html/img/button03.png"
+	document.getElementById('a2').src ="jomor_html/img/button03.png"
 }
 function mouseOut2()
 {
-document.getElementById('a2').src ="jomor_html/img/button02.png"
+	document.getElementById('a2').src ="jomor_html/img/button02.png"
 }
 /*大富翁圖片按鈕-桌遊專欄*/
 function mouseOver3()
 {
-document.getElementById('a3').src ="jomor_html/img/button05.png"
+	document.getElementById('a3').src ="jomor_html/img/button05.png"
 }
 function mouseOut3()
 {
-document.getElementById('a3').src ="jomor_html/img/button04.png"
+	document.getElementById('a3').src ="jomor_html/img/button04.png"
 }
 /*大富翁圖片按鈕-我要討論*/
 function mouseOver4()
 {
-document.getElementById('a4').src ="jomor_html/img/button07.png"
+	document.getElementById('a4').src ="jomor_html/img/button07.png"
 }
 function mouseOut4()
 {
-document.getElementById('a4').src ="jomor_html/img/button06.png"
+	document.getElementById('a4').src ="jomor_html/img/button06.png"
+}
+/*店家列表*/
+function opendiv(divID)
+		{ 
+		    //根據傳遞的參數確定顯示的層
+			divID.style.display='block';
+			divID.style.left=(document.body.clientWidth-240)/2;
+			divID.style.top=(document.body.clientHeight-139)/2;
+		}
+/*店家列表三角形變色按鍵*/
+/*變色按鍵01*/
+function store_mouseOver1()
+{
+	document.getElementById('store_triangle_id01').src ="jomor_html/img/store_triangle2.png" 
+}
+function store_mouseOut1()
+{
+	document.getElementById('store_triangle_id01').src ="jomor_html/img/store_triangle.png"
+}
+/*變色按鍵02*/
+function store_mouseOver2()
+{
+	document.getElementById('store_triangle_id02').src ="jomor_html/img/store_triangle2.png" 
+}
+function store_mouseOut2()
+{
+	document.getElementById('store_triangle_id02').src ="jomor_html/img/store_triangle.png"
+}
+/*變色按鍵03*/
+function store_mouseOver3()
+{
+	document.getElementById('store_triangle_id03').src ="jomor_html/img/store_triangle2.png" 
+}
+function store_mouseOut3()
+{
+	document.getElementById('store_triangle_id03').src ="jomor_html/img/store_triangle.png"
+}
+/*變色按鍵04*/
+function store_mouseOver4()
+{
+	document.getElementById('store_triangle_id04').src ="jomor_html/img/store_triangle2.png" 
+}
+function store_mouseOut4()
+{
+	document.getElementById('store_triangle_id04').src ="jomor_html/img/store_triangle.png"
+}
+/*變色按鍵05*/
+function store_mouseOver5()
+{
+	document.getElementById('store_triangle_id05').src ="jomor_html/img/store_triangle2.png" 
+}
+function store_mouseOut5()
+{
+	document.getElementById('store_triangle_id05').src ="jomor_html/img/store_triangle.png"
+}
+/*變色按鍵06*/
+function store_mouseOver6()
+{
+	document.getElementById('store_triangle_id06').src ="jomor_html/img/store_triangle2.png" 
+}
+function store_mouseOut6()
+{
+	document.getElementById('store_triangle_id06').src ="jomor_html/img/store_triangle.png"
 }
