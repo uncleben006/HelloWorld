3db10a1ce57833fbc22ceab354166ad1bbb44808
Ben <uncleben006@gmail.com>
2016-10-08 01:04:32 +0800

    微調，加入評價系統(尚未完成)

--- system/group/userData.php
+++ system/group/userData.php
@@ -1,19 +1,41 @@
 <?php
 include('link.php');
-$account=$_GET['account'];
+$account = $_GET['account'];
+$no = $_GET['no'];
 
-$mysql = "SELECT * FROM `user` WHERE `account` ='$account'";
+
+//取出user table裡當前帳號的資料
+$mysql1 = "SELECT * FROM `user` WHERE `account` ='$account'";
 mysql_query("SET NAMES'UTF8'");
 mysql_query("SET CHARACTER SET UTF8");
 mysql_query("SET CHARACTER_SET_RESULTS='UTF8'");
-$result = mysql_query($mysql);
-$row = mysql_fetch_assoc($result);
+$result1 = mysql_query($mysql1);
+$row1 = mysql_fetch_assoc($result1);
 
-$account = $row['account'];
-$name = $row['name'];
-$email = ['email'];
-$introduction = ['introduction'];
-$photo = ['photo'];
+$account = $row1['account'];
+$name = $row1['name'];
+$email = $row1['email'];
+$introduction = $row1['introduction'];
+$photo = $row1['photo'];
+
+$mysql2 = "SELECT * FROM `room` WHERE `no` = '".$no."'";
+$result2 = mysql_query($mysql2);
+$row2 = mysql_fetch_assoc($result2);
+$date = $row2['date'];
+$time = $row2['time'];
+$startTime = date("Y-m-d-H:i:s", strtotime($date.$time));//揪團開始時間
+
+date_default_timezone_set('Asia/Taipei');
+$now = date("Y-m-d-H:i:s");
+
+//若按judge button，則把judge的value加入資料庫中
+/*if(isset($_POST['judge'])){
+	if(){//若現在時間<揪團時間則block
+
+	}
+}*/
+
+
 
 ?>
 
@@ -173,40 +195,44 @@ $photo = ['photo'];
 <body>
 	<div class="frame">
 		<div class="room">
-			<div class="member_banner">
-				<div class="member_banner1"><?php echo "帳號" ?></div>
-				<div class="member_banner2"><?php echo $row['account'] ?></div>
-			</div>
-			<div class="room_padding">
-				<div class="room_content">
-					<div class="member_height">
-						<div class="member_title3">
-							<img src="../user/photo/<?php echo $row['photo'] ?>" height="180px"" />
-						</div>
-						<div class="room_title">
-							<div class="room_title1">暱稱</div>
-							<div class="room_title2"><?php echo $row['name'] ?></div>
-						</div>
-						<div class="room_title">
-							<div class="room_title1">信箱</div>
-							<div class="room_title2"><?php echo $row['email'] ?></div>
-						</div>
-						<div class="room_title">
-							<div class="room_title1">自我介紹</div>
-							<div class="room_title3"></div>
-							<span class="introduction"><?php echo $row['introduction'] ?></span>
-						</div>
-						<div style="clear:both"></div>
-					</div>							
-				</div>				
-			</div>
-			<div class="room_footer">
-				<div class="room_footer_redline"></div>
-				<div class="room_footer1">
-					<button class="room_footer_bottom" type="submit" onclick="history.go(-1)">回上一頁</button>
-				</div>	
-			</div>
-									
+			<form method="post" >
+				<div class="member_banner">
+					<div class="member_banner1"><?php echo "帳號" ?></div>
+					<div class="member_banner2"><?php echo $row1['account'] ?></div>
+					<input type="hidden" name="account" value="<?php echo $row1['account'] ?>">
+				</div>
+				<div class="room_padding">
+					<div class="room_content">
+						<div class="member_height">
+							<div class="member_title3">
+								<img src="../user/photo/<?php echo $row1['photo'] ?>" height="180px"" />
+							</div>
+							<div class="room_title">
+								<div class="room_title1">暱稱</div>
+								<div class="room_title2"><?php echo $row1['name'] ?></div>
+							</div>
+							<div class="room_title">
+								<div class="room_title1">信箱</div>
+								<div class="room_title2"><?php echo $row1['email'] ?></div>
+							</div>
+							<div class="room_title">
+								<div class="room_title1">自我介紹</div>
+								<div class="room_title3"></div>
+								<span class="introduction"><?php echo $row1['introduction'] ?></span>
+							</div>
+							<div style="clear:both"></div>
+						</div>							
+					</div>				
+				</div>
+				<div class="room_footer">
+					<div class="room_footer_redline"></div>
+					<div class="room_footer1">
+						<button class="room_footer_bottom" type="submit" onclick="history.go(-1)">回上一頁</button>
+						<button type="submit" class="room_footer_bottom" name="judge" value=1 title="給予此玩家在這次揪團正面的肯定">正評</button>
+						<button type="submit" class="room_footer_bottom" name="judge" value=-1 title="給予此玩家在這次揪團負面的評比">負評</button>						
+					</div>	
+				</div>
+			</form>					
 		</div>
 	</div>
 </body>
