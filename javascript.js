586502819bb5dcb9ada3b8c673ac29e1379cad27
Ben <uncleben006@gmail.com>
2016-10-08 00:51:25 +0800

    新style

--- javascript.js
+++ javascript.js
@@ -98,3 +98,45 @@ function store_mouseOut6()
 {
 	document.getElementById('store_triangle_id06').src ="jomor_html/img/store_triangle.png"
 }
+/*揪團創建房間div*/
+function openroom(divID)
+		{ 
+		    //根據傳遞的參數確定顯示的層
+			divID.style.display='block';
+			divID.style.left=(document.body.clientWidth-240)/2;
+			divID.style.top=(document.body.clientHeight-139)/2;
+		}
+/*測試測試頁籤功能*/
+$(function(){
+    // 預設顯示第一個 Tab
+    var _showTab = 0;
+    var $defaultLi = $('ul.tabs_ul li').eq(_showTab).addClass('active');
+    $($defaultLi.find('a').attr('href')).siblings().hide();
+    
+    // 當 li 頁籤被點擊時...
+    // 若要改成滑鼠移到 li 頁籤就切換時, 把 click 改成 mouseover
+    $('ul.tabs_ul li').click(function() {
+      // 找出 li 中的超連結 href(#id)
+      var $this = $(this),
+        _clickTab = $this.find('a').attr('href');
+      // 把目前點擊到的 li 頁籤加上 .active
+      // 並把兄弟元素中有 .active 的都移除 class
+      $this.addClass('active').siblings('.active').removeClass('active');
+      // 淡入相對應的內容並隱藏兄弟元素
+      $(_clickTab).stop(false, true).fadeIn().siblings().hide();
+
+      return false;
+    }).find('a').focus(function(){
+      this.blur();
+    });
+  });
+/*揪團觸發瀏覽房間所跳出的房間div*/
+function openviewroom(divID)
+		{ 
+		    //根據傳遞的參數確定顯示的層
+			divID.style.display='block';
+			divID.style.left=(document.body.clientWidth-240)/2;
+			divID.style.top=(document.body.clientHeight-139)/2;
+		}
+
+
