b9088ba4b7c74b7ff2c0354ea4b3c39ef4eca0fb
Ben <uncleben006@gmail.com>
2016-10-08 00:49:48 +0800

    資料庫檔

--- boardgame.sql
+++ boardgame.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- 主機: 127.0.0.1
--- 產生時間： 2016-09-18 16:15:22
+-- 產生時間： 2016-10-07 18:48:43
 -- 伺服器版本: 10.1.13-MariaDB
 -- PHP 版本： 5.6.23
 
@@ -46,6 +46,32 @@ INSERT INTO `admin` (`no`, `pri`, `account`, `password`, `name`, `email`, `intro
 -- --------------------------------------------------------
 
 --
+-- 資料表結構 `chat`
+--
+
+CREATE TABLE `chat` (
+  `no` int(11) DEFAULT NULL,
+  `name` char(8) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `now` time NOT NULL,
+  `chat` text COLLATE utf8_unicode_ci
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- 資料表的匯出資料 `chat`
+--
+
+INSERT INTO `chat` (`no`, `name`, `now`, `chat`) VALUES
+(38, '源源水源市場原', '22:02:39', '安安~'),
+(38, '源源水源市場原', '22:02:42', '安安~'),
+(41, '222', '13:40:35', '安安'),
+(41, '222', '13:41:02', '痾痾'),
+(41, '源源水源市場原', '21:06:31', '廢物'),
+(41, '源源水源市場原', '21:06:40', '安安'),
+(41, '源源水源市場原', '21:06:55', '我超強!');
+
+-- --------------------------------------------------------
+
+--
 -- 資料表結構 `game`
 --
 
@@ -65,17 +91,86 @@ CREATE TABLE `game` (
 -- --------------------------------------------------------
 
 --
+-- 資料表結構 `judge`
+--
+
+CREATE TABLE `judge` (
+  `judger` varchar(20) COLLATE utf8_unicode_ci NOT NULL,
+  `be judged` varchar(20) COLLATE utf8_unicode_ci NOT NULL,
+  `time` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- 資料表結構 `member`
+--
+
+CREATE TABLE `member` (
+  `no` int(3) NOT NULL,
+  `people` int(2) NOT NULL,
+  `name` text COLLATE utf8_unicode_ci NOT NULL,
+  `account` varchar(20) COLLATE utf8_unicode_ci NOT NULL,
+  `email` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `photo` varchar(100) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- 資料表的匯出資料 `member`
+--
+
+INSERT INTO `member` (`no`, `people`, `name`, `account`, `email`, `photo`) VALUES
+(41, 5, '222', '222', 'uncleben006@gmail.com', '170538.jpg'),
+(42, 0, '222', '222', 'uncleben006@gmail.com', '170538.jpg'),
+(43, 20, '222', '222', 'uncleben006@gmail.com', '170538.jpg'),
+(41, 5, '源源水源市場原', '111', 'uncleben006@gmail.com', '777658.jpg'),
+(44, 2, '', '', '', '');
+
+-- --------------------------------------------------------
+
+--
+-- 資料表結構 `remind`
+--
+
+CREATE TABLE `remind` (
+  `no` int(3) NOT NULL,
+  `account` varchar(20) COLLATE utf8_unicode_ci NOT NULL,
+  `email` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `host` varchar(20) COLLATE utf8_unicode_ci NOT NULL,
+  `room` text COLLATE utf8_unicode_ci NOT NULL,
+  `time` datetime NOT NULL,
+  `store` text COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- 資料表的匯出資料 `remind`
+--
+
+INSERT INTO `remind` (`no`, `account`, `email`, `host`, `room`, `time`, `store`) VALUES
+(1, '222', 'uncleben006@gmail.com', '555', '555', '2016-10-03 12:12:00', '#'),
+(1, '333', 'uncleben006@gmail.com', '555', '555', '2016-10-03 12:12:00', '#'),
+(1, '555', 'uncleben006@gmail.com', '555', '555', '2016-10-03 12:12:00', '#'),
+(2, '333', 'uncleben006@gmail.com', '333', '33', '2016-10-03 22:22:00', '#'),
+(2, '333', 'uncleben006@gmail.com', '333', '33', '2016-10-03 22:22:00', '#');
+
+-- --------------------------------------------------------
+
+--
 -- 資料表結構 `room`
 --
 
 CREATE TABLE `room` (
   `no` int(3) NOT NULL,
-  `host` varchar(20) NOT NULL,
+  `decide` int(2) NOT NULL,
+  `host` varchar(20) COLLATE utf8_unicode_ci NOT NULL,
   `room` text COLLATE utf8_unicode_ci NOT NULL,
   `store` text COLLATE utf8_unicode_ci NOT NULL,
+  `x` float NOT NULL,
+  `y` float NOT NULL,
   `game` text COLLATE utf8_unicode_ci NOT NULL,
   `date` date NOT NULL,
   `time` time NOT NULL,
+  `time2` time NOT NULL,
   `people` varchar(5) COLLATE utf8_unicode_ci NOT NULL,
   `spend` text COLLATE utf8_unicode_ci NOT NULL,
   `remark` text COLLATE utf8_unicode_ci NOT NULL
@@ -85,74 +180,10 @@ CREATE TABLE `room` (
 -- 資料表的匯出資料 `room`
 --
 
-INSERT INTO `room` (`no`, `room`, `store`, `game`, `date`, `time`, `people`, `spend`, `remark`) VALUES
-(16, '快進來阿肥宅! ', '#', '矮人礦坑', '2016-09-14', '13:30:00', '3', '50/hr', '無'),
-(18, '進來裡面有正妹', '#', '說謊大師', '2016-09-14', '15:00:00', '3', '30/hr', '騙你的ㄎㄎ'),
-(19, 'ㄚㄚㄚㄚㄚ', '#', '矮人礦坑', '2016-09-21', '05:30:00', '5', '50/hr', '嗚嗚嗚');
-
--- --------------------------------------------------------
-
---
--- 資料表結構 `room16`
---
-
-CREATE TABLE `room16` (
-  `people` int(2) NOT NULL,
-  `name` text NOT NULL,
-  `account` varchar(20) NOT NULL,
-  `photo` varchar(100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- 資料表的匯出資料 `room16`
---
-
-INSERT INTO `room16` (`people`, `name`, `account`, `photo`) VALUES
-(3, '222', '222', '428032.jpg'),
-(3, '幹幹幹', '333', 'fuck-you.png'),
-(3, '阿元元阿', '555', '715800.jpg'),
-(3, 'ㄚㄚㄚ', '888', 'fuck-you.png');
-
--- --------------------------------------------------------
-
---
--- 資料表結構 `room18`
---
-
-CREATE TABLE `room18` (
-  `people` int(2) NOT NULL,
-  `name` text NOT NULL,
-  `account` varchar(20) NOT NULL,
-  `photo` varchar(100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- 資料表的匯出資料 `room18`
---
-
-INSERT INTO `room18` (`people`, `name`, `account`, `photo`) VALUES
-(3, 'ㄚㄚㄚㄚㄚㄚㄚㄚㄚㄚㄚㄚㄚㄚㄚㄚㄚㄚㄚㄚㄚㄚㄚㄚㄚㄚㄚㄚㄚㄚㄚㄚㄚㄚㄚㄚㄚㄚㄚㄚㄚㄚㄚㄚㄚㄚㄚㄚㄚㄚㄚㄚㄚㄚㄚㄚㄚㄚㄚㄚㄚㄚㄚㄚㄚㄚㄚㄚㄚㄚㄚㄚㄚㄚㄚㄚㄚㄚㄚㄚㄚㄚㄚㄚㄚㄚㄚㄚㄚㄚㄚㄚㄚㄚㄚㄚㄚㄚㄚㄚㄚㄚㄚㄚㄚㄚㄚㄚㄚㄚㄚㄚㄚㄚㄚㄚㄚㄚㄚㄚㄚㄚㄚㄚㄚㄚㄚㄚㄚㄚㄚㄚㄚㄚㄚㄚㄚㄚㄚㄚㄚㄚㄚㄚㄚㄚㄚㄚㄚㄚㄚㄚㄚㄚㄚㄚㄚㄚㄚㄚㄚㄚㄚㄚㄚㄚㄚㄚㄚㄚㄚㄚㄚㄚㄚㄚㄚㄚㄚㄚㄚㄚㄚㄚㄚㄚㄚㄚㄚㄚㄚㄚㄚㄚㄚㄚㄚㄚㄚㄚㄚㄚㄚㄚㄚ', '555', '715800.jpg');
-
--- --------------------------------------------------------
-
---
--- 資料表結構 `room19`
---
-
-CREATE TABLE `room19` (
-  `people` int(2) NOT NULL,
-  `name` text NOT NULL,
-  `account` varchar(20) NOT NULL,
-  `photo` varchar(100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- 資料表的匯出資料 `room19`
---
-
-INSERT INTO `room19` (`people`, `name`, `account`, `photo`) VALUES
-(5, '222', '222', '428032.jpg'),
-(5, '安安', '555', 'fuck-you.png');
+INSERT INTO `room` (`no`, `decide`, `host`, `room`, `store`, `x`, `y`, `game`, `date`, `time`, `time2`, `people`, `spend`, `remark`) VALUES
+(41, 0, '222', '創建房間房主要在裡面', 'swan caf''e', 25.0884, 121.464, '2', '2016-10-08', '15:15:00', '00:00:00', '5', '2', '2'),
+(43, 0, '222', '來看看要是超出格式會怎樣', 'swan caf''e', 25.0884, 121.464, '3', '2016-10-08', '15:16:00', '00:00:00', '20', '3', '3'),
+(44, 0, '', '123', 'swan caf''e', 25.0884, 121.464, '2', '2016-10-08', '15:15:00', '00:00:00', '2', '2', '2\r\n');
 
 -- --------------------------------------------------------
 
@@ -176,14 +207,15 @@ CREATE TABLE `user` (
 --
 
 INSERT INTO `user` (`no`, `pri`, `account`, `password`, `name`, `email`, `introduction`, `photo`) VALUES
-('001MH', 0, '000', '000', '元', 'uncleben006@gmail.com', '沒驗證過唷沒驗證過唷沒驗證過唷沒驗證過唷沒驗證過唷沒驗證過唷沒驗證過唷沒驗證過唷沒驗證過唷沒驗證過唷沒驗證過唷沒驗證過唷沒驗證過唷沒驗證過唷沒驗證過唷沒驗證過唷沒驗證過唷沒驗證過唷沒驗證過唷沒驗證過唷沒驗證過唷沒驗證過唷沒驗證過唷沒驗證過唷沒驗證過唷沒驗證過唷沒驗證過唷沒驗證過唷沒驗證過唷沒驗證過唷沒驗證過唷沒驗證過唷沒驗證過唷沒驗證過唷沒驗證過唷沒驗證過唷沒驗證過唷沒驗證過唷沒驗證過唷沒驗證過唷沒驗證過唷沒驗證過唷', ''),
-('003MG', 1, '222', '222', '222', 'uncleben006@gmail.com', '222', '428032.jpg'),
+('003MG', 1, '222', '222', '222', 'uncleben006@gmail.com', '222', '170538.jpg'),
 ('004XL', 1, '333', '333', '幹幹幹', 'uncleben006@gmail.com', '幹幹幹', 'fuck-you.png'),
-('004GB', 1, '111', '111', '源源元', 'uncleben006@gmail.com', '源源元', ''),
 ('005JM', 0, '444', '444', '源源元源源', 'uncleben006@gmail.com', '源源元元', 'fuck-you.png'),
-('006UU', 1, '555', '555', '安安', 'uncleben006@gmail.com', '安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安', 'fuck-you.png'),
-('', 1, '', '', '', '', '', ''),
-('008XS', 0, '888', '888', 'ㄚㄚㄚ', 'uncleben006@gmail.com', 'ㄚㄚㄚ', 'fuck-you.png');
+('006UU', 1, '555', '555', '安安', 'uncleben006@gmail.com', '安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安安', 'fuck-you.png'),
+('008XS', 0, '888', '888', 'ㄚㄚㄚ', 'uncleben006@gmail.com', 'ㄚㄚㄚ', 'fuck-you.png'),
+('006YK', 1, '111', '111', '源源水源市場原', 'uncleben006@gmail.com', '元你妹啦~', '777658.jpg'),
+('007FW', 1, '777', '777', '元元', 'uncleben004@gmail.com', '源源元', '45724.jpg'),
+('008PF', 1, 'abc', 'abc', 'abc', 'uncleben006@gmail.com', 'abc', '660161.jpg'),
+('009YF', 0, '000', '000', '111', 'uncleben006@gmail.com', '源源', '634155.jpg');
 
 --
 -- 已匯出資料表的索引
@@ -202,24 +234,6 @@ ALTER TABLE `room`
   ADD PRIMARY KEY (`no`);
 
 --
--- 資料表索引 `room16`
---
-ALTER TABLE `room16`
-  ADD PRIMARY KEY (`account`);
-
---
--- 資料表索引 `room18`
---
-ALTER TABLE `room18`
-  ADD PRIMARY KEY (`account`);
-
---
--- 資料表索引 `room19`
---
-ALTER TABLE `room19`
-  ADD PRIMARY KEY (`account`);
-
---
 -- 在匯出的資料表使用 AUTO_INCREMENT
 --
 
@@ -232,7 +246,7 @@ ALTER TABLE `game`
 -- 使用資料表 AUTO_INCREMENT `room`
 --
 ALTER TABLE `room`
-  MODIFY `no` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+  MODIFY `no` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=45;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
