de43929e9d21ea8b4427676258cf815ec4c80fa6
Ben <uncleben006@gmail.com>
2016-10-11 10:17:56 +0800

    加入situation2

--- system/user/block.php
+++ system/user/block.php
@@ -15,13 +15,28 @@
 	</head>
 	<body>
 		<div class="frame">
-			<h1>你尚未登入唷~~</h1>
-			<h2>三秒後會轉跳至登入頁面</h2>
+			<?php
+			if(isset($_GET['situation'])){
+				$situation = $_GET['situation'];
+				if($situation==1){
+					?>
+					<h1>你尚未登入唷~~</h1>
+					<h2>三秒後會轉跳至登入頁面</h2>
+					<?php
+					header("refresh:3;login.php");				
+				}
+				else if($situation==2){
+					?>
+					<h1>你登入失敗了唷~~</h1>
+					<h2>三秒後會轉跳至登入頁面</h2>
+					<?php
+					header("refresh:3;login.php");
+				}
+			}
+			?>
+			
 		</div>
 		
 	</body>
 </html>
 
-<?php
-	header("refresh:3;login.html");
-?>
