d147fd48ac9ba05a172fc354757ce7f8e61f6def
Ben <uncleben006@gmail.com>
2016-10-14 20:57:01 +0800

    include sessionCheck.php

--- blog.php
+++ blog.php
@@ -3,24 +3,24 @@
 <head>
 	<title>jomor桌末狂歡</title>
 	<link href="style.css" rel="stylesheet" type="text/css" />
-	<link rel="stylesheet" type="text/css" href="flexslider.css" />
+	<script type="text/javascript" src="javascript.js"></script>
 	<meta charset="utf-8">
 </head>
 	<body id="body0">
-	
-		<!--把header放入header.php方便管理-->
-		<?php include("header.php"); ?>
+		<?php include('include/sessionCheck.php') ?>
+		<?php include('include/header.php') ?>
 		<section class="blue_section">	<!--藍底大象-->
 			<div class="blue_bg">
-				<img class="blog_img" src="jomor_html/img/blogimg.png" height="275px" width="720px">
+				<img class="blog_img" src="jomor_html/img/blogimg.png" height="240px" width="690px">
 			</div>
 		</section>
+		<!--
 		<section>
-			<div class="blogbutton_frame">	<!--頁籤按鈕-->
+			<div class="blogbutton_frame">	//頁籤按鈕
 				<a href="#" class="blog_myButton">熱門文章</a>
 				<a href="#" class="blog_myButton">最新文章</a>
 			</div>
-		</section>
+		</section>-->
 		<!--文章區塊-->
 		<section class="essay_section">
 			<div class="essay_fram"><!--div表格法(第一篇的藍底區塊）-->
@@ -57,7 +57,7 @@
 						</div>
 						<div class="eassy_artical_div"><!--文章部分內文區塊-->
 							<div class="eassy_artical">到底是哪時款桌遊入選呢..............好期待好期待～.............</div>
-							<div><a href="blog_more.php" class="read_eassy_Button">閱讀全文</a></div>
+							<div><a href="blog_more.html" class="read_eassy_Button">閱讀全文</a></div>
 						</div>
 					</div>
 				</div>
@@ -76,7 +76,7 @@
 						</div>
 						<div class="eassy_artical_div"><!--文章部分內文區塊-->
 							<div class="eassy_artical">哇哇哇哇哇嗚～～～～好期待好期待～到底是哪時款桌遊入選呢.............</div>
-							<div><a href="blog_more.php" class="read_eassy_Button">閱讀全文</a></div>
+							<div><a href="blog_more.html" class="read_eassy_Button">閱讀全文</a></div>
 						</div>
 					</div>
 				</div>
@@ -95,7 +95,7 @@
 						</div>
 						<div class="eassy_artical_div"><!--文章部分內文區塊-->
 							<div class="eassy_artical">哇哇哇哇哇嗚～～～～好期待好期待～到底是哪時款桌遊入選呢.............</div>
-							<div><a href="blog_more.php" class="read_eassy_Button">閱讀全文</a></div>
+							<div><a href="blog_more.html" class="read_eassy_Button">閱讀全文</a></div>
 						</div>
 					</div>
 				</div>
