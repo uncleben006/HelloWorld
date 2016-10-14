8dad282685e06dd10434949a436044bf03ad2a19
Ben <uncleben006@gmail.com>
2016-10-15 00:19:41 +0800

    新sql

--- boardgame.sql
+++ boardgame.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- 主機: 127.0.0.1
--- 產生時間： 2016-10-11 08:56:08
+-- 產生時間： 2016-10-14 18:19:21
 -- 伺服器版本: 10.1.13-MariaDB
 -- PHP 版本： 5.6.23
 
@@ -64,7 +64,8 @@ INSERT INTO `chat` (`no`, `name`, `now`, `chat`) VALUES
 (38, '源源水源市場原', '22:02:39', '安安~'),
 (38, '源源水源市場原', '22:02:42', '安安~'),
 (57, '222', '01:22:21', '安安'),
-(57, '安安', '01:31:52', '幹嘛?');
+(57, '安安', '01:31:52', '幹嘛?'),
+(59, '安安', '01:28:27', '安安');
 
 -- --------------------------------------------------------
 
@@ -117,9 +118,8 @@ CREATE TABLE `member` (
 --
 
 INSERT INTO `member` (`no`, `people`, `name`, `account`, `email`, `photo`) VALUES
-(59, 3, '超過五個字', '333', 'uncleben006@gmail.com', 'iMac-icon.png'),
-(60, 5, '超過五個字', '333', 'uncleben006@gmail.com', 'iMac-icon.png'),
-(60, 5, '222', '222', 'uncleben006@gmail.com', '170538.jpg');
+(8, 3, '阿元元阿222', '222', 'uncleben006@gmail.com', '170538.jpg'),
+(8, 3, '你怎麼沒暱稱', '111', 'uncleben006@gmail.com', 'fuck-you.png');
 
 -- --------------------------------------------------------
 
@@ -133,9 +133,19 @@ CREATE TABLE `remind` (
   `email` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
   `host` varchar(20) COLLATE utf8_unicode_ci NOT NULL,
   `room` text COLLATE utf8_unicode_ci NOT NULL,
-  `time` datetime NOT NULL,
+  `date` date NOT NULL,
+  `time` time NOT NULL,
   `store` text COLLATE utf8_unicode_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- 資料表的匯出資料 `remind`
+--
+
+INSERT INTO `remind` (`no`, `account`, `email`, `host`, `room`, `date`, `time`, `store`) VALUES
+(98, '222', 'uncleben006@gmail.com', '222', 'header可以用了RRR', '2016-10-13', '15:16:00', 'swan caf''e'),
+(98, '111', 'uncleben006@gmail.com', '222', 'header可以用了RRR', '2016-10-13', '15:16:00', 'swan caf''e'),
+(98, '777', 'uncleben004@gmail.com', '222', 'header可以用了RRR', '2016-10-13', '15:16:00', 'swan caf''e');
 
 -- --------------------------------------------------------
 
@@ -165,8 +175,7 @@ CREATE TABLE `room` (
 --
 
 INSERT INTO `room` (`no`, `decide`, `host`, `room`, `store`, `x`, `y`, `game`, `date`, `time`, `time2`, `people`, `spend`, `remark`) VALUES
-(59, 0, '333', '這是一間房間', 'swan caf''e', 25.0884, 121.464, '5', '2016-10-12', '15:16:00', '00:00:00', '3', '5', '5'),
-(60, 0, '333', '這是第二個房間', 'swan caf''e', 25.0884, 121.464, '5', '2016-10-13', '15:16:00', '00:00:00', '5', '5', '5');
+(8, 0, '222', '源源源源', 'swan caf''e', 25.0884, 121.464, '矮人礦坑', '2016-10-15', '15:16:00', '15:16:00', '3', '30/hr', '2016');
 
 -- --------------------------------------------------------
 
@@ -182,21 +191,24 @@ CREATE TABLE `user` (
   `name` text COLLATE utf8_unicode_ci NOT NULL,
   `email` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
   `introduction` text COLLATE utf8_unicode_ci NOT NULL,
-  `photo` varchar(100) COLLATE utf8_unicode_ci NOT NULL
+  `photo` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `gender` text COLLATE utf8_unicode_ci NOT NULL,
+  `favorite` text COLLATE utf8_unicode_ci NOT NULL,
+  `goodAt` text COLLATE utf8_unicode_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
 -- 資料表的匯出資料 `user`
 --
 
-INSERT INTO `user` (`no`, `pri`, `account`, `password`, `name`, `email`, `introduction`, `photo`) VALUES
-('003MG', 1, '222', '222', '222', 'uncleben006@gmail.com', '222', '170538.jpg'),
-('007FW', 1, '777', '777', '元元', 'uncleben004@gmail.com', '源源元', '45724.jpg'),
-('008PF', 1, 'abc', 'abc', 'abc', 'uncleben006@gmail.com', 'abc', '660161.jpg'),
-('009YF', 0, '000', '000', '111', 'uncleben006@gmail.com', '源源', '634155.jpg'),
-('010UN', 0, 'benny', 'benny', 'Ben', 'uncleben006@gmail.com', '我喜歡虧妹', '813724.jpg'),
-('006GQ', 0, '111', '111', '安安', 'uncleben006@gmail.com', '', 'iMac-icon.png'),
-('007XJ', 0, '333', '333', '超過五個字', 'uncleben006@gmail.com', '', 'iMac-icon.png');
+INSERT INTO `user` (`no`, `pri`, `account`, `password`, `name`, `email`, `introduction`, `photo`, `gender`, `favorite`, `goodAt`) VALUES
+('003MG', 1, '222', '222', '阿元元阿222', 'uncleben006@gmail.com', '你想知道什麼?', '170538.jpg', '', '諜影危機', '沒有QQ幫QQ'),
+('007FW', 1, '777', '777', '元元', 'uncleben004@gmail.com', '源源元', '45724.jpg', '', '', ''),
+('008PF', 1, 'abc', 'abc', 'abc', 'uncleben006@gmail.com', 'abc', '660161.jpg', '', '', ''),
+('009YF', 0, '000', '000', '111', 'uncleben006@gmail.com', '源源', '634155.jpg', '', '', ''),
+('010UN', 0, 'benny', 'benny', 'Ben', 'uncleben006@gmail.com', '我喜歡虧妹', '813724.jpg', '', '', ''),
+('006GQ', 0, '111', '111', '你怎麼沒暱稱', 'uncleben006@gmail.com', '哈哈哈', 'fuck-you.png', '', '', ''),
+('007XJ', 0, '333', '333', '超過五個字', 'uncleben006@gmail.com', '', 'iMac-icon.png', '', '', '');
 
 --
 -- 已匯出資料表的索引
@@ -227,7 +239,7 @@ ALTER TABLE `game`
 -- 使用資料表 AUTO_INCREMENT `room`
 --
 ALTER TABLE `room`
-  MODIFY `no` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=61;
+  MODIFY `no` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
