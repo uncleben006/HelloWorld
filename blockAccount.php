b51f5ab1aa5a595d37454e10ffd9e8666569cc7b
Ben <uncleben006@gmail.com>
2016-10-17 17:41:41 +0800

    10/17

---
+++ blockAccount.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>jomor桌末狂歡</title>
+	<link href="style.css" rel="stylesheet" type="text/css" />
+	<script type="text/javascript" src="javascript.js"></script>
+	<meta charset="utf-8">
+</head>
+	<body id="body0">
+		<?php
+			include('include/sessionCheck.php');
+			header("refresh:3;system/user/login.php");
+			include("include/header.php"); 
+		?>
+		<img src="jomor_html/img/accountCheck.png" class="discussion_img">
+	</body>
+</html>
