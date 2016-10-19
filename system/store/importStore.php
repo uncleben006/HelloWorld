2776428b1f3d870e185e32f66ef30e880dc0bf67
Ben <uncleben006@gmail.com>
2016-10-19 19:57:45 +0800

    10/19

--- system/store/importStore.php
+++ system/store/importStore.php
@@ -1,9 +1,33 @@
-</!DOCTYPE html>
+<!DOCTYPE html>
 <html>
 <head>
-	<title>import</title>
+	<title>jomor桌末狂歡</title>
+	<script type="text/javascript" src="http://code.jquery.com/jquery-latest.min.js"></script>
+	<link href="../../style.css" rel="stylesheet" type="text/css" />
+	<script type="text/javascript" src="../../javascript.js"></script>
+	<script src="jquery.min.js"></script>	
+	<meta charset="utf-8">
 </head>
-<body>
-
-</body>
+	<body id="body0">
+		<?php include('../../include/storeHeader.php'); ?>
+		<section><!--註冊框-->
+			<div class="register_div">
+				<div class="register_bg"><!--註冊黃色框-->
+					<div class="register_white"><!--註冊白色框-->
+						<div class="register_p_div">
+							<h1 class="register_p">管理員資料上傳後台</h1>
+						</div>
+						<hr color="#A0920D" size="3" width="95%">
+						<div>
+							<h2>店家名稱</h2>
+							<input type="text" name="store" class="import_text">
+						</div>
+							
+						
+						
+					</div>
+				</div>
+			</div>
+		</section>
+	</body>
 </html>
