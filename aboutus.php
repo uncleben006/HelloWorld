d978bcb5eb0d5fb99f0bbd3b393f3b24c245b38a
Ben <uncleben006@gmail.com>
2016-10-24 15:51:28 +0800

    10/24

--- aboutus.php
+++ aboutus.php
@@ -22,70 +22,8 @@
 			</div>
 		</section>
 		<section>
-			<div class="aboutus_photo_div1"><!--個人介紹表格-->
-				<div class="aboutus_tr"><!--tr-->
-					<div class="aboutus_td"><!--td-->
-						<img src="jomor_html/img/about1.png" height="360px" width="200px">
-					</div>
-					<div class="position">專案<br>經理</div><!--td-->
-					<div class="work_td">
-						<div class="work1">
-							<p class="work_p1">&nbsp;&nbsp;&nbsp;預算管理<br>&nbsp;&nbsp;&nbsp;企劃撰寫<br>專案進度管理</p>
-						</div>
-					</div>
-				</div>					
-			</div>
-			<div class="aboutus_photo_div2"><!--個人介紹表格-->
-				<div class="aboutus_tr"><!--tr-->
-					<div class="aboutus_td"><!--td-->
-						<img src="jomor_html/img/about2.png" height="360px" width="200px">
-					</div>
-					<div class="position">網頁<br>設計</div><!--td-->
-					<div class="work_td">
-						<div class="work1">
-							<p class="work_p2">&nbsp;&nbsp;&nbsp;網站架構設計<br>&nbsp;&nbsp;&nbsp;前端語言撰寫</p>
-						</div>
-					</div>
-				</div>					
-			</div>
-			<div class="aboutus_photo_div2"><!--個人介紹表格-->
-				<div class="aboutus_tr"><!--tr-->
-					<div class="aboutus_td"><!--td-->
-						<img src="jomor_html/img/about3.png" height="360px" width="200px">
-					</div>
-					<div class="position">程式<br>設計</div><!--td-->
-					<div class="work_td">
-						<div class="work1">
-							<p class="work_p2">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;系統設計<br>&nbsp;&nbsp;&nbsp;後端語言撰寫</p>
-						</div>
-					</div>
-				</div>					
-			</div>
-			<div class="aboutus_photo_div2"><!--個人介紹表格-->
-				<div class="aboutus_tr"><!--tr-->
-					<div class="aboutus_td"><!--td-->
-						<img src="jomor_html/img/about4.png" height="360px" width="200px">
-					</div>
-					<div class="position">行銷<br>公關</div><!--td-->
-					<div class="work_td">
-						<div class="work1">
-							<p class="work_p2">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;網路行銷<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;活動企劃</p>
-						</div>
-					</div>
-				</div>					
-			</div>
-			<div class="aboutus_photo_div3"><!--個人介紹表格-->
-				<div class="aboutus_tr"><!--tr-->
-					<div class="aboutus_td"><!--td-->
-						<img src="jomor_html/img/about5.png" height="260px" width="200px">
-					</div>
-					<div class="position2">美術<br>設計</div><!--td-->
-					<div class="work_td">
-						<div class="work2">
-							<p class="work_p2">&nbsp;&nbsp;&nbsp;圖文美工編輯<br>&nbsp;&nbsp;&nbsp;網站介面設計</p>
-						</div>
-					</div>
-				</div>					
+			<div class="aboutall">
+				<img src="jomor_html/img/aboutusall.png" width="500px" height="1300px">
 			</div>
 		</section>
 </body>
